10fd9557da5db0cee92388d9cd8490c9cb95b5ef
Vladislav Pavlov <vlad.z.pavlov@gmail.com>
2024-01-14 17:57:32 +0200

    add icon

--- src/components/DropDown/Dropdown.jsx
+++ src/components/DropDown/Dropdown.jsx
@@ -1,5 +1,5 @@
 import { NavLink } from 'react-router-dom';
-import { FaInfoCircle, FaChartBar, FaBars } from 'react-icons/fa';
+import { FaInfoCircle, FaChartLine, FaChartBar, FaPeopleCarry, FaProjectDiagram, FaBars, FaFilter } from 'react-icons/fa';
 import useDropDown from '../../hooks/useDropDown';
 import './Dropdown.css';
 
@@ -9,7 +9,7 @@ function Dropdown({ data, isStatisticComponent, isHeaderComponent, handleFilterC
   return (
     <div ref={dropdownRef} className="dropdown">
       <button className="dropdown__toggle" onClick={toggleDropdown}>
-        {isStatisticComponent && 'Filter'}
+        {isStatisticComponent && <><FaFilter />Filter</>}
         {isHeaderComponent && <FaBars className='burger__icon' />}
       </button>
       {isDropdownOpen &&
@@ -17,20 +17,20 @@ function Dropdown({ data, isStatisticComponent, isHeaderComponent, handleFilterC
           {isStatisticComponent && (
             <>
               <NavLink to={"/employees-statistic/overall-stats"} state={{ data: data }}>
-                Overall Statistic
+                <FaChartLine /> Overall Statistic
               </NavLink>
               <NavLink to={"/employees-statistic/worked-project-period"} state={{ data: data }}>
-                Project Period
+                <FaProjectDiagram/> Project Period
               </NavLink>
               <NavLink to={"/employees-statistic/worked-together-period"} state={{ data: data }}>
-                Worked Together Period
+                <FaPeopleCarry /> Worked Together Period
               </NavLink>
             </>
           )}
           {isHeaderComponent && (
             <>
-              <NavLink to={"/"}><FaInfoCircle /> Home</NavLink>
-              <NavLink to={"/employees-statistic"}><FaChartBar /> Statistic</NavLink>
+              <NavLink to={"/"}><FaInfoCircle />Home</NavLink>
+              <NavLink to={"/employees-statistic"}><FaChartBar />Statistic</NavLink>
             </>
           )}
         </nav>
--- src/components/FileInput/FileInput.jsx
+++ src/components/FileInput/FileInput.jsx
@@ -1,10 +1,11 @@
+import { FaUpload } from "react-icons/fa";
 import './FileInput.css';
 
 function FileInput({ onChange }) {
 
     return (
         <div className="label__container">
-            <label htmlFor="file">Choose File</label>
+            <label htmlFor="file"><FaUpload className="label_icon"/>Choose File</label>
             <input type="file" onChange={onChange} />
         </div>
     );
